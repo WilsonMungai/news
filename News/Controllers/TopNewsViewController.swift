c6cefa1cca81ac954530f46d26344a0f461aaa1d
WilsonMungai <wilsonmuguthi@gmail.com>
2023-02-24 12:17:54 -0500

    added top stories table view

--- News/Controllers/TopNewsViewController.swift
+++ News/Controllers/TopNewsViewController.swift
@@ -8,11 +8,40 @@
 import UIKit
 
 class TopNewsViewController: UIViewController {
-
+    
+    private let topNewsTable: UITableView = {
+        let table = UITableView(frame: .zero, style: .grouped)
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return table
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         view.backgroundColor = .systemBackground
+        
+        view.addSubview(topNewsTable)
+        
+        topNewsTable.dataSource = self
+        topNewsTable.delegate = self
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        topNewsTable.frame = view.bounds
     }
 
+}
+
+extension TopNewsViewController: UITableViewDelegate, UITableViewDataSource {
+    // data source function
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    // delegate function
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.backgroundColor = .systemBackground
+        return cell
+    }
 }
